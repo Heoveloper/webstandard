dceabc1b3a64e559fee576530052b6fd0067c6be
Junhyeok Heo <106042161+Heoveloper@users.noreply.github.com>
2022-08-18 15:39:59 +0900

    module practice

---
+++ module/js/a.js
@@ -0,0 +1,9 @@
+import {minus, multi, var3 as var333} from './b.js';
+
+let var1 = 1;
+let var2 = 2;
+const add = (x, y) => x+y;
+
+export default add;
+
+console.log(minus(var1, var333));   //1(a.js) - 4(b.js) = -3
---
+++ module/js/c.js
@@ -0,0 +1,7 @@
+import add from './a.js';
+
+let var1 = 10;
+let var2 = 20;
+console.log(var1);
+const sum = add(var1, var2);
+console.log(sum);
