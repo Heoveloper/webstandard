7e3f5f49f2a15f67f1cde67cdbef3e52209c7c48
Junhyeok Heo <106042161+Heoveloper@users.noreply.github.com>
2022-08-23 17:21:48 +0900

    keys n values

--- object/test1.js
+++ object/test1.js
@@ -4,3 +4,13 @@ const person = {
     blood: 'A',
     address: { nation: '한국', location: '울산' }
 }
+
+Object.keys(person).forEach((key, idx, arr) => {
+    console.log(key);
+});
+Object.values(person).forEach((value, idx, arr) => {
+    console.log(value);
+});
+Object.entries(person).forEach((entry, idx, arr) => {
+    console.log(entry);
+});
